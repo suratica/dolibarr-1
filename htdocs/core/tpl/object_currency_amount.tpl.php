af925191c9a1c395d165af9481195c6b651e3cc6
Frédéric France <frederic.france@free.fr>
2024-11-19 23:37:10 +0100

    test

--- htdocs/core/tpl/object_currency_amount.tpl.php
+++ htdocs/core/tpl/object_currency_amount.tpl.php
@@ -24,8 +24,13 @@
  * $langs
  */
 /**
+ * @var CommonObject $object
+ * @var Conf $conf
  * @var Form $form
  * @var Translate $langs
+ *
+ * @var string $action
+ * @var int $usercancreate
  */
 // Protection to avoid direct call of template
 if (empty($conf) || !is_object($conf)) {
