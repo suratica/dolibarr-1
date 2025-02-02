57ccb41c6ef5b26c9392388fdce514efaf772a98
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2025-02-02 20:09:47 +0100

    docker-dev: remove files rights change in docker-run.sh
    File rights from the whole environment should not change since the files
    are belonging to the git tree. In addition, setting www-data to the same
    user id as the user (or likewise, setting a userns remapping) remove the
    need to change the rights.

--- dev/build/docker-dev/docker-run.sh
+++ dev/build/docker-dev/docker-run.sh
@@ -2,18 +2,13 @@
 # Script used by the Dockerfile.
 # See README.md to know how to create a Dolibarr env with docker
 
+if [ "${HOST_USER_ID}" == "" ]; then
+	echo "Define HOST_USER_ID to your user ID before starting"
+	exit 1
+fi
+
 usermod -u "${HOST_USER_ID}" www-data
 groupmod -g "${HOST_USER_ID}" www-data
-
-chgrp -hR www-data /var/www/html
-chmod g+rwx /var/www/html/conf
-
-if [ ! -d /var/documents ]; then
-	echo "[docker-run] => create volume directory /var/documents ..."
-	mkdir -p /var/documents
-fi
-echo "[docker-run] => Set Permission to www-data for /var/documents"
-chown -R www-data:www-data /var/documents
 
 echo "[docker-run] => update '${PHP_INI_DIR}/conf.d/dolibarr-php.ini'"
 cat <<EOF > "${PHP_INI_DIR}/conf.d/dolibarr-php.ini"
