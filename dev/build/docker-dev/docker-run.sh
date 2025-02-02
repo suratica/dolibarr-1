2b9deda89b4a85a9d24aa62087f8245d1406154f
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2025-02-02 20:09:50 +0100

    docker-dev: README: set document width to 80
    Improve terminal reading experience by limiting the document width to
    80. On the web interface, markdown with merge every line into a single
    paragraph.

--- dev/build/docker-dev/docker-run.sh
+++ dev/build/docker-dev/docker-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script used by the Dockerfile.
 # See README.md to know how to create a Dolibarr env with docker
+set -ex
 
 if [ "${HOST_USER_ID}" == "" ]; then
 	echo "Define HOST_USER_ID to your user ID before starting"
@@ -14,6 +15,7 @@ echo "[docker-run] => update '${PHP_INI_DIR}/conf.d/dolibarr-php.ini'"
 cat <<EOF > "${PHP_INI_DIR}/conf.d/dolibarr-php.ini"
 date.timezone = ${PHP_INI_DATE_TIMEZONE:-UTC}
 memory_limit = ${PHP_INI_MEMORY_LIMIT:-256M}
+display_errors = Off
 EOF
 
 exec apache2-foreground
