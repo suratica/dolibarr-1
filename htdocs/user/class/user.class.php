fe3669621a216aadc15709110f5c3fa483668247
Laurent Destailleur (aka Eldy) <eldy@destailleur.fr>
2025-02-12 19:10:36 +0100

    NEW Add option MAIN_MAIL_EMAIL_FROM_PASSWORDRESET

--- htdocs/user/class/user.class.php
+++ htdocs/user/class/user.class.php
@@ -2663,12 +2663,12 @@ class User extends CommonObject
 		$outputlangs = new Translate("", $conf);
 
 		if (isset($this->conf->MAIN_LANG_DEFAULT)
-			&& $this->conf->MAIN_LANG_DEFAULT != 'auto') {	// If user has defined its own language (rare because in most cases, auto is used)
-			$outputlangs->getDefaultLang($this->conf->MAIN_LANG_DEFAULT);
+			&& getDolGlobalString('MAIN_LANG_DEFAULT') != 'auto') {	// If user has defined its own language (rare because in most cases, auto is used)
+			$outputlangs->getDefaultLang(getDolGlobalString('MAIN_LANG_DEFAULT'));
 		}
 
-		if ($this->conf->MAIN_LANG_DEFAULT) {
-			$outputlangs->setDefaultLang($this->conf->MAIN_LANG_DEFAULT);
+		if (getDolGlobalString('MAIN_LANG_DEFAULT')) {
+			$outputlangs->setDefaultLang(getDolGlobalString('MAIN_LANG_DEFAULT'));
 		} else {	// If user has not defined its own language, we used current language
 			$outputlangs = $langs;
 		}
@@ -2729,7 +2729,7 @@ class User extends CommonObject
 		$mailfile = new CMailFile(
 			$subject,
 			$this->email,
-			$conf->global->MAIN_MAIL_EMAIL_FROM,
+			getDolGlobalString("MAIN_MAIL_EMAIL_FROM_PASSWORDRESET", getDolGlobalString("MAIN_MAIL_EMAIL_FROM")),
 			$mesg,
 			array(),
 			array(),
