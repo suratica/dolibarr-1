d81c124b77f585cbf1c06365dd55a0ea2493f32e
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2025-02-28 11:55:32 +0100

    Fix setup default value

--- htdocs/core/class/html.formsetup.class.php
+++ htdocs/core/class/html.formsetup.class.php
@@ -700,10 +700,10 @@ class FormSetupItem
 	{
 		global $conf;
 		if (isset($conf->global->{$this->confKey})) {
-			$this->fieldValue = getDolGlobalString($this->confKey);
+			$this->fieldValue = getDolGlobalString($this->confKey, null);
 			return true;
 		} else {
-			$this->fieldValue = '';
+			$this->fieldValue = null;
 			return false;
 		}
 	}
