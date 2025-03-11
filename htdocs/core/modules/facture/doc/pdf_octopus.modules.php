c0f533865f0cd252f04aed3b1537700d50857f46
MDW <mdeweerd@users.noreply.github.com>
2025-03-11 01:42:49 +0100

    Fix CI: Do not require some fields in the 'title' definition of $cols (#33399)
    * Qual: Fix PhanMismatchType
    
    * Finalize rename of $attrV into $value
    
    * Correct getGlobalString into getGlobalInt
    
    * Do not require some fields in the 'title' definition of $cols

--- htdocs/core/modules/facture/doc/pdf_octopus.modules.php
+++ htdocs/core/modules/facture/doc/pdf_octopus.modules.php
@@ -118,7 +118,7 @@ class pdf_octopus extends ModelePDFFactures
 
 
 	/**
-	 * @var array<string,array{rank:int,width:float|int,status:bool,title:array{textkey:string,label:string,align:string,padding:array{0:float,1:float,2:float,3:float}},content:array{align:string,padding:array{0:float,1:float,2:float,3:float}}}>	Array of document table columns
+	 * @var array<string,array{rank:int,width:float|false,status:bool|int<0,1>,border-left?:bool,title:array{textkey:string,label?:string,align?:string,padding?:array{0:float,1:float,2:float,3:float}},content?:array{align?:string,padding?:array{0:float,1:float,2:float,3:float}}}>	Array of document table columns
 	 */
 	public $cols;
 
@@ -2614,7 +2614,7 @@ class pdf_octopus extends ModelePDFFactures
 		$rank += 10;
 		$this->cols['photo'] = array(
 			'rank' => $rank,
-			'width' => getDolGlobalString('MAIN_DOCUMENTS_WITH_PICTURE_WIDTH', 20), // in mm
+			'width' => getDolGlobalInt('MAIN_DOCUMENTS_WITH_PICTURE_WIDTH', 20), // in mm
 			'status' => false,
 			'title' => array(
 				'textkey' => 'Photo',
