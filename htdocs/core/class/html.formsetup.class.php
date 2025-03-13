8449961d9e98bb18ef3dd40674075989c3443022
ldestailleur <eldy@destailleur.fr>
2025-03-13 23:12:31 +0100

    FIX #33435 Warnings

--- htdocs/core/class/html.formsetup.class.php
+++ htdocs/core/class/html.formsetup.class.php
@@ -906,8 +906,8 @@ class FormSetupItem
 		} elseif ($this->type == 'yesno') {
 			if (!empty($conf->use_javascript_ajax)) {
 				$input = $this->fieldParams['input'] ?? array();
-				$revertonoff = $this->fieldParams['revertonoff'] ? 1 : 0;
-				$forcereload = $this->fieldParams['forcereload'] ? 1 : 0;
+				$revertonoff = !empty($this->fieldParams['revertonoff']) ? 1 : 0;
+				$forcereload = !empty($this->fieldParams['forcereload']) ? 1 : 0;
 
 				$out .= ajax_constantonoff($this->confKey, $input, $this->entity, $revertonoff, 0, $forcereload);
 			} else {
