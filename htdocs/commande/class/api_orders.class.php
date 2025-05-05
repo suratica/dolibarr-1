704910b4b3b031ce447496edc1c7e3314ad3baea
Charlène Benke <1179011+defrance@users.noreply.github.com>
2025-05-05 02:07:24 +0200

    NEW Add internal contact of proposal, order and invoice on api call (#34009)
    * add internal contact of elements
    
    * Update api_orders.class.php
    
    * Update api_invoices.class.php
    
    * Update api_proposals.class.php
    
    * Update api_orders.class.php
    
    * Fix CI
    
    * Update api_orders.class.php
    
    * Update api_invoices.class.php
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/commande/class/api_orders.class.php
+++ htdocs/commande/class/api_orders.class.php
@@ -134,11 +134,18 @@ class Orders extends DolibarrApi
 			throw new RestException(403, 'Access not allowed for login '.DolibarrApiAccess::$user->login);
 		}
 
-		// Add external contacts ids
-		$tmparray = $this->commande->liste_contact(-1, 'external', $contact_list);
-		if (is_array($tmparray)) {
-			$this->commande->contacts_ids = $tmparray;
+		if ($contact_list > -1) {
+			// Add external contacts ids
+			$tmparray = $this->commande->liste_contact(-1, 'external', $contact_list);
+			if (is_array($tmparray)) {
+				$this->commande->contacts_ids = $tmparray;
+			}
+			$tmparray = $this->commande->liste_contact(-1, 'internal', $contact_list);
+			if (is_array($tmparray)) {
+				$this->commande->contacts_ids_internal = $tmparray;
+			}
 		}
+
 		$this->commande->fetchObjectLinked();
 
 		// Add online_payment_url, cf #20477
