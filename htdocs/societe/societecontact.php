568b47cb4af1cf433f3643bb13684a507010e538
ldestailleur <eldy@destailleur.fr>
2025-09-16 12:33:04 +0200

    Clean code

--- htdocs/societe/societecontact.php
+++ htdocs/societe/societecontact.php
@@ -76,7 +76,7 @@ if ($user->socid) {
 }
 
 // Initialize a technical object to manage hooks of page. Note that conf->hooks_modules contains an array of hook context
-$hookmanager->initHooks(array('thirdpartycontact', 'globalcard'));
+$hookmanager->initHooks(array('thirdpartycontact', 'thirdpartycontactcard', 'globalcard'));
 
 $result = restrictedArea($user, 'societe', $id, '');
 
